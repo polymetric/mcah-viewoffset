e1b4203f8e4ed477581f8fce03fc97c534f9730e
polymetric <polymetricofficial@gmail.com>
2021-01-25 19:27:44 -0500

    initial commit

---
+++ src/main/java/net/nodium/mcmods/viewoffset/ViewOffsetMod.java
@@ -0,0 +1,28 @@
+package net.nodium.mcmods.viewoffset;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class ViewOffsetMod implements ModInitializer {
+	private static KeyBinding toggleOffset;
+
+    @Override
+    public void onInitialize() {
+		toggleOffset = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.viewoffset.toggle",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_U,
+				"category.viewoffset"
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (toggleOffset.wasPressed()) {
+				client.player.pitch -= 0.15;
+			}
+		});
+    }
+}
